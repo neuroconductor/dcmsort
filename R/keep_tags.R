01a9fac8273d2fe00da6acdf8ca2372c1beb9d76
muschellij2 <muschellij2@gmail.com>
2019-02-19 23:04:42 -0500

    Added exposure.

--- R/keep_tags.R
+++ R/keep_tags.R
@@ -7,13 +7,16 @@
 #' @examples
 #' relevant_tags()
 relevant_tags = function() {
-  c("(0008,0008)", "(0008,0060)", "(0018,1210)",
+  x = c("(0008,0008)", "(0008,0060)", "(0018,1210)",
     "(0018,1160)", "(0018,1151)", "(0018,0081)",
     "(0018,1150)", "(0018,0080)", "(0008,9007)",
     "(0018,9316)", "(0018,0050)", "(0018,0010)",
     "(0028,0030)", "(0008,2112)", "(0008,103e)",
     "(0008,1030)", "(0028,1050)", "(0028,1051)",
     "(0028,1055)",
+    "(0018,1152)", # Exposure
+    "(0018,0010)", # Contrast/Bolus Agent
+    # http://dicom.nema.org/medical/dicom/current/output/chtml/part03/sect_C.7.6.4.html
     "(0028,1050)", # WindowCenter
     "(0028,1051)", # WindowWidth
     "(0008,0050)", #AccessionNumber
@@ -21,6 +24,8 @@ relevant_tags = function() {
     "(0020,0011)" # SeriesNumber
 
   )
+  x = unique(x)
+  x
 }
 
 #' @export
